90f1dd25edf2167fab0284850a8746f08325e3be
talentorigin <talentorigin@gmail.com>
2017-07-01 15:44:37 +0530

    gizip files to hive data comparison

--- src/GzipFiles.scala
+++ src/GzipFiles.scala
@@ -1,33 +1,67 @@
 
 
-import scala.util.Try
-
-import org.apache.commons.compress.compressors.gzip.GzipCompressorInputStream
-import org.apache.spark.input.PortableDataStream
-import org.apache.spark.sql.SparkSession
 import java.io.BufferedReader
 import java.io.InputStreamReader
 
+import org.apache.commons.compress.compressors.gzip.GzipCompressorInputStream
+import org.apache.spark.input.PortableDataStream
+import org.apache.spark.sql.Row
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.types.StringType
+import org.apache.spark.sql.types.StructField
+import org.apache.spark.sql.types.StructType
+
 object GzipFiles {
+
   val spark = SparkSession.builder()
     .appName("GZip files Testing")
-    .master("local")
+    .master("local[*]")
+    .enableHiveSupport()
     .getOrCreate()
+
   def main(args: Array[String]): Unit = {
     val byteBuffer = 1024
 
-    val rdd = spark.sparkContext.binaryFiles("sample_data/")
+    val rdd = spark.sparkContext.binaryFiles("/user/cloudera/bank_customer/")
       .flatMap {
         case (fileName: String, pds: PortableDataStream) =>
           extractFiles(pds)
       }
 
-    val headers = rdd.filter(_.startsWith("customer_identifier"))
-    headers.foreach(println)
+    val hiveSchema = spark.sqlContext.table("bank_customer").schema
+    val colNames = hiveSchema.fieldNames
 
-    val fileContents = rdd.filter(!_.startsWith("customer_identifier"))
-    println("Header Count: " + headers.count)
-    println("Contents Count: " + fileContents.count)
+    val stringSchema = StructType(hiveSchema.map {
+      field =>
+        StructField(field.name, StringType, true)
+    })
+
+    stringSchema.printTreeString()
+
+    val rowRDD = rdd.filter(!_.startsWith("customer_identifier")).map {
+      line =>
+        val lineArray = line.split(",")
+        val lineRDD = generateRow(lineArray)
+        lineRDD
+    } //map
+
+    val tempDF = spark.sqlContext.createDataFrame(rowRDD, stringSchema)
+    val sourceDF = applySchema(tempDF, hiveSchema)
+    sourceDF.printSchema()
+    sourceDF.show()
+
+    val targetDF = spark.sqlContext.sql("select * from bank_customer")
+
+    println("Check Status: " + sourceDF.except(targetDF).count)
+  }
+
+  def applySchema(df: org.apache.spark.sql.DataFrame, schema: StructType): org.apache.spark.sql.DataFrame = {
+    var tempDF = df
+    schema.foreach {
+      field =>
+        tempDF = tempDF.withColumn(field.name, tempDF(field.name).cast(field.dataType))
+    } //foreach
+    tempDF
   }
 
   def extractFiles(pds: PortableDataStream): List[String] = {
@@ -35,5 +69,7 @@ object GzipFiles {
     val br = new BufferedReader(new InputStreamReader(gzipStream, "UTF-8"))
     Stream.continually(br.readLine).takeWhile(_ != null).toList
   }
+
+  def generateRow(lineArray: Array[String]): Row = Row.fromSeq(lineArray)
 
 }
